6fff8ac55f5ebf047b4824c15dbce3eaced4b812
shtrvv <shatrova.liza.21@gmail.com>
2024-01-25 21:16:06 +0200

    upd hw

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key {
+    private signature: number = Math.random();
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {
+        this.key = key;
+    }
+
+    getKey() {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected tenants: Person[] = [];
+
+    constructor(protected key: Key) {
+        this.key = key;
+    }
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (this.key.getSignature() === key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
